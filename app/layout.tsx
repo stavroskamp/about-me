540beecc0005ee803d50c54d99b16ac2e047a8b5
Stavros Kampanakis <stavros.kampanakis@gmail.com>
2025-11-10 18:08:18 +0200

    trying usermetric analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,58 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* CookieYes CMP (banner + consent updates) */}
+        {/* Usermetric Pixel */}
         <Script
-          id="cookieyes-banner"
-          src="https://app.cookieyes.com/mHHtKTYII3w39cbnfPBcoM-7avywBGTHsI2gDtD76sAe2ncyehVxmtKG9xzuEh4uaerkcJ7ZQ_nne4KKqPAWPIYmI9ODvUZbkMhciOW5uvivSoaDMzrBQ77NJ8TIFsW-kSTqvs7EeQS2eLYgSl6VnztJFbTqQDn-y0uqNr1K4uI="
-          strategy="beforeInteractive"
+          src="https://app.usermetric.io/pixel/jnVhUNQRS3ulIeJ8"
+          strategy="afterInteractive"
+          defer
         />
-        <Script
-          id="cookieyes-main"
-          src="https://cdn-cookieyes.com/client_data/7245a91df182fda2972fe99c/script.js"
-          strategy="beforeInteractive"
-        />
-
-        {/* Google Consent Mode default: everything denied until user accepts */}
-        <Script id="gtag-consent" strategy="beforeInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){ dataLayer.push(arguments); }
-            gtag('consent', 'default', {
-              ad_storage: 'denied',
-              analytics_storage: 'denied'
-            });
-          `}
-        </Script>
-
-        {/* Google Tag Manager */}
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NXZDTJSB');
-          `}
-        </Script>
       </head>
-      <body className={`${inter.className} bg-[#0f172a]`}>
-        {/* GTM noscript fallback */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NXZDTJSB"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-            title="GTM"
-          />
-        </noscript>
-        {children}
-      </body>
+      <body className={`${inter.className} bg-[#0f172a]`}>{children}</body>
     </html>
   );
 }
